cdac46649241d5c581638e6e4430e92a44975465
Jacob Bradshaw <bra12067@byui.edu>
2018-03-05 10:16:19 -0700

    Revert "Topic 9 complete"
    This reverts commit 03ef9958c60a8b54c9053dd95faf3a4700b091e6.

--- Topic 9 - JavaScript Events/Topic 9 scripts.js
+++ Topic 9 - JavaScript Events/Topic 9 scripts.js
@@ -6,11 +6,11 @@ function getTime() {
 window.addEventListener('load', function() {
 	var touch = document.getElementById('touchy');
 
-	var colors = ["blue", "green", "red", "yellow", "orange", "pink", "purple", "aqua", "black", "teal", "burgundy", "#41f444", "#b2f441", "#f141f4", "#f4418e", "#f4d041"];
+	var colors = ["blue", "green", "red", "yellow", "orange", "pink", "purple", "aqua", "black", "teal", "burgundy"];
 
 	touch.addEventListener('touchstart', function(e) {
 		document.getElementById('textOut').innerHTML = "You Touched me";
-		touch.style.backgroundColor = colors[Math.floor((Math.random()*16)+1)];
+		touch.style.backgroundColor = colors[Math.floor((Math.random()*11)+1)];
 
 
 		//var touchObj = e.changedTouches[];
